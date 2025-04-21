b0d5f56b2474e75e3228d371ff973d83405a6e9e
jaeweon <00cjw@naver.com>
2025-04-21 19:32:26 +0900

    hyper parameter eddit

--- utility/yoloow_utils.py
+++ utility/yoloow_utils.py
@@ -12,23 +12,27 @@ with use_model_root("YoloOW"):
 from torch.optim.lr_scheduler import CosineAnnealingLR
 from coco_eval import CocoEvaluator
 from utility.optimizer import build_optimizer
+import yaml
 
 def build_yoloow_model(cfg, ex_dict=None):
     cfg_path = Path(cfg)
-    # 1차: 호출자가 절대경로를 줬을 때
     if cfg_path.is_file():
         pass
-    # 2차: 프로젝트 기본 위치
     else:
         cfg_path = _MODEL_ROOTS["YoloOW"] / "cfg" / "training" / cfg
-        if not cfg_path.is_file():                       # ← ❗ 여기서만 예외 처리
+        if not cfg_path.is_file():                     
             raise FileNotFoundError(f"[YoloOW] config file not found: {cfg_path}")
 
     print(f"[YoloOW] Using config → {cfg_path}")         # 디버그 로그
     # 하이퍼 파라미터 세팅
     model = YoloOWModel(cfg=str(cfg_path), ch=3, nc=ex_dict['Number of Classes']).to(ex_dict['Device'])
-    if ex_dict and "Hyp" in ex_dict:
-        model.hyp = ex_dict["Hyp"]
+    hyp_path = _MODEL_ROOTS["YoloOW"] / "data" / "hyp.scratch.p5.yaml"
+    with open(hyp_path, 'r') as f:
+        model.hyp = yaml.safe_load(f)
+
+    # gradient ratio
+    model.gr = 1.0 #defualt
+    
     return model
 
 def train_yoloow_model(ex_dict):
